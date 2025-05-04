3e96c6fca56491cec9c8ae3074550a7d5dff959e
Jober Moegele Correa <jobernc@gmail.com>
2025-05-04 20:53:50 +0200

    Switch to Yarn, fix specifier.charCodeAt error, and update dependency resolutions

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,64 @@
+const webpack = require('webpack');
+
 module.exports = function override(config) {
-    // Adicionar polyfills para módulos Node.js
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "http": require.resolve("stream-http"),
-      "url": require.resolve("url/"),
-      "zlib": require.resolve("browserify-zlib"),
-      "fs": false,
-      "stream": require.resolve("stream-browserify"),
-      "path": require.resolve("path-browserify")
-    };
-    return config;
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    assert: require.resolve('assert/'),
+    crypto: require.resolve('crypto-browserify'),
+    querystring: require.resolve('querystring'),
+    timers: require.resolve('timers-browserify'),
+    stream: require.resolve('stream-browserify'),
+    path: require.resolve('path-browserify'),
+    os: require.resolve('os-browserify/browser'),
+    zlib: require.resolve('browserify-zlib'),
+    http: require.resolve('https-browserify'),
+    https: require.resolve('https-browserify'),
+    fs: false,
+    net: false,
+    buffer: require.resolve('buffer/'),
+    util: require.resolve('util/'),
+    events: require.resolve('events-browserify'),
+    vm: require.resolve('vm-browserify'),
+    async_hooks: false
   };
+
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    }),
+    new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
+      const mod = resource.request.replace(/^node:/, '');
+      if (mod === 'events') {
+        resource.request = 'events-browserify';
+      } else if (mod === 'fs' || mod === 'http' || mod === 'net' || mod === 'async_hooks') {
+        resource.request = false;
+      } else if (mod === 'path') {
+        resource.request = 'path-browserify';
+      } else if (mod === 'zlib') {
+        resource.request = 'browserify-zlib';
+      } else if (mod === 'util') {
+        resource.request = 'util/';
+      } else if (mod === 'buffer') {
+        resource.request = 'buffer/';
+      } else if (mod === 'vm') {
+        resource.request = 'vm-browserify';
+      }
+    }),
+    new webpack.NormalModuleReplacementPlugin(/express/, (resource) => {
+      resource.request = false;
+    }),
+    new webpack.NormalModuleReplacementPlugin(/on-finished/, (resource) => {
+      resource.request = false;
+    }),
+    new webpack.NormalModuleReplacementPlugin(/raw-body/, (resource) => {
+      resource.request = false;
+    }),
+    new webpack.NormalModuleReplacementPlugin(/querystring-es3/, (resource) => {
+      resource.request = 'querystring';
+    })
+  ];
+
+  return config;
+};
--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import MissionManager from './components/MissionManager';
 import GameManager from './components/GameManager';
 
 const client = new Client()
-  .setEndpoint('https://fra.cloud.appwrite.io/v1')
-  .setProject('681d9c6b092fe2d2bc3f')
-  .setKey('standard_9c19b4026e029462c99a814ebece86adbb4e2fd147027ea825da09d58a2104cf3c3b4b9f32ad4742c13a09895d72babdb323d67b59401a8a7a84481db404ded369ce95c66215923a50de610199e9135702a6680390e1d5bd626b8c65a9dcd63593243852c4d4c0b5c6f672a7ad610a01f14c8c809449bb8d855afbd4007138e7');
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(process.env.REACT_APP_APPWRITE_PROJECT_ID)
+  .setKey(process.env.REACT_APP_APPWRITE_API_KEY);
 
 function App() {
   return (
